52dc016ce2b309ee588abb55e66dec9355916325
Vivek Ramini <30451871+raminivivek@users.noreply.github.com>
2019-02-03 22:33:55 -0600

    Update HomeController.cs

--- GitHubTestWebApp/GitHubTestWebApp/Controllers/HomeController.cs
+++ GitHubTestWebApp/GitHubTestWebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace GitHubTestWebApp.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
+			ViewBag.Message = "Your application  page.";
 
 			return View();
 		}
